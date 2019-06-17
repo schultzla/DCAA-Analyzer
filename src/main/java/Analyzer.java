50a95a8bbdfb3fd84324692f99f732ff42635032
Logan <schultzlogana@gmail.com>
2019-06-17 16:02:18 -0400

    Cleaned up dramatically, finished implementing filters feature, add documentation

--- src/main/java/Analyzer.java
+++ src/main/java/Analyzer.java
@@ -5,66 +5,35 @@ import java.util.*;
 
 public class Analyzer {
 
-    HashMap<String, HashSet<String>> employeeSubmissions = new HashMap<>();
-    Map<String, Integer> employeeLateCounts = new TreeMap<>();
-    HashMap<String, Record> records = new HashMap<>();
-    HashMap<String, Double> totalAttempts = new HashMap<>();
-    TreeSet<String> jobCodes = new TreeSet<>();
+    static HashMap<String, HashSet<String>> employeeSubmissions = new HashMap<>();
+    static Map<String, Integer> employeeLateCounts = new TreeMap<>();
+    static HashMap<String, Record> records = new HashMap<>();
+    static HashMap<String, Double> totalAttempts = new HashMap<>();
+    static TreeSet<String> jobCodes = new TreeSet<>();
+
+    List<Record> data;
 
     public Analyzer(String fileName) {
-        for (Record r : Poiji.fromExcel(new File(fileName), Record.class)) {
+        data = Poiji.fromExcel(new File(fileName), Record.class);
+        for (Record r : data) {
             jobCodes.add(r.jobCode);
-
-            if (totalAttempts.containsKey(r.username)) {
-                totalAttempts.put(r.username, totalAttempts.get(r.username) + 1.0);
-            } else {
-                totalAttempts.put(r.username, 1.0);
-            }
-
-            if (r.jobCode.contains("Holiday") || r.jobCode.contains("Leave") || r.jobCode.contains("Vacation") || r.jobCode.contains("Sick")) {
-                continue;
-            } else {
-                if (r.compareDates()) {
-                    records.put(r.username, r);
-                    SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-                    String simpleJobDate = sdf.format(r.dateJob.toDate());
-
-                    if (employeeSubmissions.containsKey(r.username)) {
-                        if (!employeeSubmissions.get(r.username).contains(simpleJobDate)) {
-                            employeeSubmissions.get(r.username).add(simpleJobDate);
-                            if (employeeLateCounts.containsKey(r.username)) {
-                                employeeLateCounts.put(r.username, employeeLateCounts.get(r.username) + 1);
-                            } else {
-                                employeeLateCounts.put(r.username, 1);
-                            }
-                        }
-                    } else {
-                        HashSet<String> temp = new HashSet<>();
-                        temp.add(simpleJobDate);
-                        employeeSubmissions.put(r.username, temp);
-
-                        if (employeeLateCounts.containsKey(r.username)) {
-                            employeeLateCounts.put(r.username, employeeLateCounts.get(r.username) + 1);
-                        } else {
-                            employeeLateCounts.put(r.username, 1);
-                        }
-                    }
-                }
-            }
         }
     }
 
-    public Analyzer(String fileName, HashSet<String> exemptions) {
-        for (Record r : Poiji.fromExcel(new File(fileName), Record.class)) {
-            jobCodes.add(r.jobCode);
+    public void analyze(HashSet<String> exempt) {
+        employeeSubmissions.clear();
+        employeeLateCounts.clear();
+        records.clear();
+        totalAttempts.clear();
 
+        for (Record r : data) {
             if (totalAttempts.containsKey(r.username)) {
                 totalAttempts.put(r.username, totalAttempts.get(r.username) + 1.0);
             } else {
                 totalAttempts.put(r.username, 1.0);
             }
 
-            if (exemptions.contains(r.jobCode)) {
+            if (exempt.contains(r.jobCode)) {
                 continue;
             } else {
                 if (r.compareDates()) {
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,64 +1,159 @@
 import com.poiji.exception.InvalidExcelFileExtension;
+
 import javax.swing.*;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 public class Driver {
 
     static String path;
     static int minLates = 0;
-    static Analyzer configAnalyzer;
+    static Analyzer analyzer;
     static JCheckBox[] boxes;
-    static HashSet<String> defaultCodes = new HashSet<>();
+    static HashSet<String> defaultCodes = new HashSet<>(), codes = new HashSet<>();
 
     public static void main(String[] args) {
         defaultCodes = new HashSet<String>(Arrays.asList(new String[]{"Vacation", "Holiday", "Sick", "Leave without Pay", "Ownership Vacation"}));
 
+        /*
+         * Display file chooser on launch, don't close until file chosen
+         */
+        while (path == null) {
+            JFileChooser fileChooser = new JFileChooser();
+            int returnValue = fileChooser.showOpenDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                path = selectedFile.getAbsolutePath();
+            }
+        }
+
+        /*
+         * Panels
+         */
         JPanel middlePanel = new JPanel ();
-        middlePanel.setBorder ( new TitledBorder( new EtchedBorder (), "DCAA Analyzer" ) );
-
-        JTextArea data = new JTextArea ( 16, 45 );
-        data.setEditable ( false ); // set textArea non-editable
-        JScrollPane scroll = new JScrollPane ( data );
-        scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+        JPanel filterPanel = new JPanel();
         JPanel btmPanel = new JPanel();
-        btmPanel.setBorder ( new TitledBorder( new EtchedBorder (), "Options" ) );
 
-        JButton chooseFile = new JButton("Choose File");
+        middlePanel.setBorder(new TitledBorder(new EtchedBorder(), "DCAA Analyzer"));
+        btmPanel.setBorder(new TitledBorder(new EtchedBorder(), "Options"));
+
+        /*
+         * Intialize static analyzer
+         */
+        while (true) {
+            try {
+                analyzer = new Analyzer(path);
+                break;
+            } catch (InvalidExcelFileExtension ex) {
+                JOptionPane.showMessageDialog(middlePanel, "Invalid file, must be .xlsx or xlx");
+                JFileChooser fileChooser = new JFileChooser();
+                int returnValue = fileChooser.showOpenDialog(null);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
+                    File selectedFile = fileChooser.getSelectedFile();
+                    path = selectedFile.getAbsolutePath();
+                }
+            }
+        }
+
+
+        /*
+         * Scrollable data area
+         */
+        JTextArea data = new JTextArea (16, 45);
+        data.setEditable (false);
+        JScrollPane scroll = new JScrollPane(data);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+        /*
+         * Buttons
+         */
         JButton calculateLateDays = new JButton("Calculate");
         JButton setMinLates = new JButton("Set Min Lates");
         JButton configure = new JButton("Configure");
         JButton addFilters = new JButton("Jobcode Filter");
         JButton saveFilters = new JButton("Save");
 
-        JPanel filterPanel = new JPanel();
-
-        btmPanel.add(chooseFile);
         btmPanel.add(calculateLateDays);
         btmPanel.add(configure);
 
-        middlePanel.add ( scroll );
+        middlePanel.add (scroll);
 
-        JFrame frame = new JFrame ();
+        /*
+         * Frame
+         */
+        JFrame frame = new JFrame();
         frame.setLayout(new BorderLayout());
         frame.setTitle("DCAA Analyzer");
-        frame.add ( middlePanel, BorderLayout.CENTER);
+        frame.add (middlePanel, BorderLayout.CENTER);
         frame.add(btmPanel, BorderLayout.SOUTH);
-        frame.pack ();
+        frame.pack();
         frame.setResizable(false);
-        frame.setLocationRelativeTo ( null );
-        frame.setVisible ( true );
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        /**
-         * Buttion actions below
+        /*
+        Build configuration panel/frame
          */
+        JFrame configureFrame = new JFrame("Config");
+        JPanel configurePanel = new JPanel();
+        configurePanel.setBorder(new TitledBorder(new EtchedBorder(), "Configurations"));
 
+        configureFrame.add(configurePanel);
+        configureFrame.setResizable(false);
+
+        configurePanel.add(setMinLates);
+        configureFrame.setLocationRelativeTo(frame);
+        configurePanel.add(addFilters);
+
+        configureFrame.pack();
+
+        /*
+        Build filters frame/panel
+         */
+        JFrame filters = new JFrame("Customize Filters");
+        filterPanel.setBorder(new TitledBorder(new EtchedBorder(), "Filters"));
+        filterPanel.setLayout(new BoxLayout(filterPanel, BoxLayout.Y_AXIS));
+
+        filters.add(filterPanel);
+
+        filters.setLocationRelativeTo(frame);
+
+        /*
+        Build buttons
+        */
+        boxes = new JCheckBox[analyzer.getCodes().size()];
+        String[] arrCodes = new String[analyzer.getCodes().size()];
+
+        int j = 0;
+        for (String s : analyzer.getCodes()) {
+            arrCodes[j] = s;
+            j++;
+        }
+
+        for(int i = 0; i < boxes.length; i++) {
+            boxes[i] = new JCheckBox(arrCodes[i]);
+        }
+
+        for(JCheckBox b : boxes) {
+            if (defaultCodes.contains(b.getText())) {
+                b.setSelected(true);
+            }
+            filterPanel.add(b);
+        }
+
+        filters.setResizable(false);
+
+        filterPanel.add(saveFilters);
+        filters.pack();
+
+        /*
+         * Button actions
+         */
         setMinLates.addActionListener((ActionEvent e) -> {
             try {
                 minLates = Integer.valueOf(JOptionPane.showInputDialog("Enter the minimum amount of late entries someone needs to be displayed"));
@@ -67,112 +162,42 @@ public class Driver {
             }
         });
 
-        chooseFile.addActionListener((ActionEvent e) -> {
-            JFileChooser fileChooser = new JFileChooser();
-            int returnValue = fileChooser.showOpenDialog(null);
-            if (returnValue == JFileChooser.APPROVE_OPTION) {
-                File selectedFile = fileChooser.getSelectedFile();
-                path = selectedFile.getAbsolutePath();
-            }
-        });
-
         configure.addActionListener((ActionEvent e) -> {
-            if (path == null) {
-                JOptionPane.showMessageDialog(middlePanel, "You must select a file prior to editing configurations", "Select File Error", JOptionPane.ERROR_MESSAGE);
-                chooseFile.doClick();
-            } else {
-                configAnalyzer = new Analyzer(path);
-                filterPanel.removeAll();
-                JFrame configureFrame = new JFrame("Config");
-                JPanel configurePanel = new JPanel();
-                configurePanel.setBorder(new TitledBorder(new EtchedBorder(), "Configurations"));
+            configureFrame.setVisible(true);
 
-                configureFrame.add(configurePanel);
-                configureFrame.setResizable(false);
-                configureFrame.setVisible(true);
-
-                configurePanel.add(setMinLates);
-                configureFrame.setLocationRelativeTo(frame);
-                configurePanel.add(addFilters);
-
-                boxes = new JCheckBox[configAnalyzer.getCodes().size()];
-                String[] codes = new String[configAnalyzer.getCodes().size()];
-
-                int j = 0;
-                for (String s : configAnalyzer.getCodes()) {
-                    codes[j] = s;
-                    j++;
-                }
-
-                for(int i = 0; i < boxes.length; i++) {
-                    boxes[i] = new JCheckBox(codes[i]);
-                }
-
-                for(JCheckBox b : boxes) {
-                    if (defaultCodes.contains(b.getText())) {
-                        b.setSelected(true);
-                    }
-                    filterPanel.add(b);
-                }
-
-
-                configureFrame.pack();
-            }
         });
 
         addFilters.addActionListener((ActionEvent e) -> {
-            JFrame filters = new JFrame("Customize Filters");
-            filterPanel.setBorder(new TitledBorder(new EtchedBorder(), "Filters"));
-            filterPanel.setLayout(new BoxLayout(filterPanel, BoxLayout.Y_AXIS));
-
-            filterPanel.add(saveFilters);
-            filters.add(filterPanel);
-
+            codes.clear();
             filters.setVisible(true);
-            filters.setLocationRelativeTo(frame);
-            filters.pack();
-
 
             saveFilters.addActionListener((ActionEvent ev) -> {
-                filters.dispose();
-            });
-
-        });
-
-        calculateLateDays.addActionListener((ActionEvent e) -> {
-            if (path != null) {
-                data.setText("");
-                Analyzer analyzer = null;
-                HashSet<String> codes = new HashSet<>();
-
+                /*
+                 * Build exemption filters for jobcodes
+                 */
                 for (JCheckBox b : boxes) {
                     if (b.isSelected()) {
                         codes.add(b.getText());
                     }
                 }
 
-                try {
-                    analyzer = new Analyzer(path, codes);
-                } catch (InvalidExcelFileExtension ex) {
-                    JOptionPane.showMessageDialog(middlePanel, "Invalid file, must be .xlsx or .xls", "Wrong File Type", JOptionPane.ERROR_MESSAGE);
-                    return;
-                }
-                for (String s : analyzer.employeeLateCounts.keySet()) {
-                    if (analyzer.employeeLateCounts.get(s) >= minLates) {
-                        Record temp = analyzer.getRecord(s);
-                        String name = temp.firstName + " " + temp.lastName;
+                filters.dispose();
+            });
 
-                        data.append("Employee: " + name + ", Late Entries: " + analyzer.employeeLateCounts.get(s));
-                        data.append("\n");
-                    }
-                }
-            } else {
-                JOptionPane.showMessageDialog(frame,
-                        "You must select a file first",
-                        "Error",
-                        JOptionPane.WARNING_MESSAGE);
-                chooseFile.doClick();
+        });
 
+        calculateLateDays.addActionListener((ActionEvent e) -> {
+            data.setText("");
+            analyzer.analyze(codes);
+
+            for (String s : analyzer.employeeLateCounts.keySet()) {
+                if (analyzer.employeeLateCounts.get(s) >= minLates) {
+                    Record temp = analyzer.getRecord(s);
+                    String name = temp.firstName + " " + temp.lastName;
+
+                    data.append("Employee: " + name + ", Late Entries: " + analyzer.employeeLateCounts.get(s));
+                    data.append("\n");
+                }
             }
         });
     }
